442795a8a98510c64545e3d8310c819ede96a690
catabetancur0719 <catalina.betancur0719@gmail.com>
2025-01-15 19:12:30 -0500

    Ejercicios 1 y 2 con solucion

---
+++ ej1.js
@@ -0,0 +1,12 @@
+/*
+Declarar una variable nombre
+Declarar una variable apellido
+Declarar una variable edad
+Declarar una variable fecha de nacimiento utilizando camel case
+Declarar una variable direccion */
+
+var nombre;
+var apellido;
+var edad;
+var fechaDeNacimiento;
+var direccion;
---
+++ ej2.js
@@ -0,0 +1,18 @@
+/* Crear un documento con el nombre ej2.js
+Declarar las siguientes variables de forma individual:
+nombre
+apellido
+edad
+fecha de nacimiento
+direccion
+Asignar un valor a cada variable con tus datos personales:
+Tu nombre
+Tu apellido
+Tu edad
+Tu fecha de nacimiento
+Tu direccion*/
+var nombre = 'Catalina';
+var apellido =  'Betancur';
+var edad = 31;
+var fechaDeNacimiento ='19-07-1993';
+var direccion = 'Medellin, Colombia';
